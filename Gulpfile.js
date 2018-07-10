b90d93ba349cebc555bc9968d4b347e80b71ad2f
EDWIN ORBES <yovany.dev@gmail.com>
2018-07-10 11:56:12 -0500

    Agregando IUs con Yo-yo

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,7 +4,7 @@ var rename = require('gulp-rename');
 var babel = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-
+var watchify = require('watchify');
 
 //Tarea para procesar sass
 gulp.task('styles', function () {
@@ -22,15 +22,40 @@ gulp.task('assets',function () {
 		.pipe(gulp.dest('public'));
 })
 
-//Tarea para procesar el javascript del cliente
-gulp.task('scripts', function () {
-	browserify('src/index.js')
-		.transform(babel)
-		.bundle()
-		.pipe(source('index.js')) //Pasa de browserify a gulp para seguir procesandolo
-		.pipe(rename('app.js'))
-		.pipe(gulp.dest('public'))
+//Función para
+function compile(watch) {
+	var bundle = watchify(browserify('src/index.js'));
+	
+	//Función para procesar el javascript del cliente
+	function rebundle() {
+		bundle
+			.transform(babel)
+			.bundle()
+			.pipe(source('index.js')) //Pasa de browserify a gulp para seguir procesandolo
+			.pipe(rename('app.js'))
+			.pipe(gulp.dest('public'))
+	}
+	
+	if(watch) {
+		//Detecta el evento update sobre el archivo que se esta escuchando (index.js)
+		bundle.on('update', function() {
+			console.log('==> Bundling...');
+			rebundle();
+		})
+	}
+
+	rebundle();
+}
+
+//Tarea para construir los elementos del bundle una sola vez
+gulp.task('build', function() {
+	return compile();
+})
+
+//Tarea para observar cambios en el bundle-js y construir si se requiere
+gulp.task('watch', function() {
+	return compile(true);
 })
 
 //Lanza diferentes tareas establecidas en el Array
-gulp.task('default',['styles','assets','scripts']);
+gulp.task('default',['styles','assets','build']);
